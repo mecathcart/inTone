2f9588ac686ebdb018f361de815af63f3f411fbf
Emmy Cathcart <maryellencathcart@gmail.com>
2013-01-04 13:52:31 -0500

    stress test function begun, it now plays an audio file and inserts an image.

--- public/src/StressTest.js
+++ public/src/StressTest.js
@@ -1,16 +1,23 @@
 
-/* Initial hello world test will eventually eliminate*/
-var StressTest = function(){
-	console.log("This is the stress test");
-	return true;
-};
 
 /* Play sound check*/
-
 var soundCheck = function(){
    playAudioFile("sound_check");
 };
 
+/* Play sound check*/
 var playAudioFile = function(divid) {
     document.getElementById(divid).play();
+};
+
+/* Start Stress Test*/
+var stressTest = function(){
+	
+	/* Play stimuli*/
+	playAudioFile("stress_test_stimuli");
+
+	/* Inserts Image*/
+	var DOM_img = document.createElement("img");
+	DOM_img.src = "./../audio_stimuli/stressTest/believe.gif";
+	stress_test_image.appendChild(DOM_img);
 };
