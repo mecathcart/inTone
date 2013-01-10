6459f6abea0f7875c77b66f53e06e5556afb84f1
Emmy Cathcart <maryellencathcart@gmail.com>
2013-01-10 14:52:23 -0500

    plays different stimuli every time you press the stress test button

--- public/src/StressTest.js
+++ public/src/StressTest.js
@@ -10,18 +10,40 @@ var playAudioFile = function(divid) {
     document.getElementById(divid).play();
 };
 
+var stressStimuli = [
+	"./../audio_stimuli/stressTest/believe_full.wav",
+	"./../audio_stimuli/stressTest/biology_full.wav",
+	"./../audio_stimuli/stressTest/blanket_full.wav",
+	"./../audio_stimuli/stressTest/critical_full.wav",
+	"./../audio_stimuli/stressTest/energy_full.wav",
+	"./../audio_stimuli/stressTest/evaporate_full.wav",
+	"./../audio_stimuli/stressTest/paper_full.wav",
+	"./../audio_stimuli/stressTest/politician_full.wav",
+	"./../audio_stimuli/stressTest/procedure_full.wav",
+	"./../audio_stimuli/stressTest/reputation_full.wav",
+	"./../audio_stimuli/stressTest/today_full.wav",
+	"./../audio_stimuli/stressTest/tomato_full.wav",
+]
+
+/* Stress Stimuli Index*/
+var i = 0;
+
+/* Inserts audio stimuli*/
+var stressAudio = document.createElement("audio");
+stressAudio.id = "stress_test_stimuli";
+audio_div.appendChild(stressAudio);
+
+
 /* Start Stress Test*/
 var stressTest = function(){
-	
-	/* Inserts audio stimuli*/
-	var stressAudio = document.createElement("audio");
-	stressAudio.id = "stress_test_stimuli";
-	for(var i =0; i< 12; i++){
-		stressAudio.src = stressStimuli[i];
-		audio_div.appendChild(stressAudio);
-		playAudioFile("stress_test_stimuli");
-	}
-	
+		
+
+	/* Plays audio stimuli*/	
+	var sample = stressStimuli[i];
+	stressAudio.src = sample;
+	playAudioFile("stress_test_stimuli");
+	i++;
+		
 	/* Inserts Image*/
 	var stressImg = document.createElement("img");
 	stressImg.src = "./../audio_stimuli/stressTest/believe.gif";
@@ -44,17 +66,4 @@ var syllable2 = function(){
 };
 
 
-var stressStimuli = [
-	"./../audio_stimuli/stressTest/believe_full.wav",
-	"./../audio_stimuli/stressTest/biology_full.wav",
-	"./../audio_stimuli/stressTest/blanket_full.wav",
-	"./../audio_stimuli/stressTest/critical_full.wav",
-	"./../audio_stimuli/stressTest/energy_full.wav",
-	"./../audio_stimuli/stressTest/evaporate_full.wav",
-	"./../audio_stimuli/stressTest/paper_full.wav",
-	"./../audio_stimuli/stressTest/politician_full.wav",
-	"./../audio_stimuli/stressTest/procedure_full.wav",
-	"./../audio_stimuli/stressTest/reputation_full.wav",
-	"./../audio_stimuli/stressTest/today_full.wav",
-	"./../audio_stimuli/stressTest/tomato_full.wav",
-]
+
