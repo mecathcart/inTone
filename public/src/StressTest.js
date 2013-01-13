2870ec7231c037ea94a1f006a178009639d47166
Emmy Cathcart <maryellencathcart@gmail.com>
2013-01-13 08:49:48 -0500

    new images with syllables lined up

--- public/src/StressTest.js
+++ public/src/StressTest.js
@@ -20,18 +20,18 @@ function StressStimuli(audio,image,numSyllables,stressSyllable){
 
 }
 
-var believe = new StressStimuli( "./../audio_stimuli/stressTest/believe_full.wav","./../audio_stimuli/stressTest/believe.gif",2,2);
-var biology = new StressStimuli( "./../audio_stimuli/stressTest/biology_full.wav","./../audio_stimuli/stressTest/biology.gif",4,2);
-var blanket = new StressStimuli( "./../audio_stimuli/stressTest/blanket_full.wav","./../audio_stimuli/stressTest/blanket.gif",2,1);
-var critical = new StressStimuli( "./../audio_stimuli/stressTest/critical_full.wav","./../audio_stimuli/stressTest/critical.gif",3,1);
-var energy = new StressStimuli( "./../audio_stimuli/stressTest/energy_full.wav","./../audio_stimuli/stressTest/energy.gif",3,1);
-var evaporate = new StressStimuli( "./../audio_stimuli/stressTest/evaporate_full.wav","./../audio_stimuli/stressTest/evaporate.gif",4,2);
-var paper = new StressStimuli( "./../audio_stimuli/stressTest/paper_full.wav","./../audio_stimuli/stressTest/paper.gif",2,1);
-var politician = new StressStimuli( "./../audio_stimuli/stressTest/politician_full.wav","./../audio_stimuli/stressTest/politician.gif",4,3);
-var procedure = new StressStimuli( "./../audio_stimuli/stressTest/procedure_full.wav","./../audio_stimuli/stressTest/procedure.gif",3,2);
-var reputation = new StressStimuli( "./../audio_stimuli/stressTest/reputation_full.wav","./../audio_stimuli/stressTest/reputation.gif",4,3);
-var today = new StressStimuli( "./../audio_stimuli/stressTest/today_full.wav","./../audio_stimuli/stressTest/today.gif",2,2);
-var tomato = new StressStimuli( "./../audio_stimuli/stressTest/tomato_full.wav","./../audio_stimuli/stressTest/tomato.gif",3,2);
+var believe = new StressStimuli( "./../audio_stimuli/stressTest/believe_full.wav","./../stress_images/believe.gif",2,2);
+var biology = new StressStimuli( "./../audio_stimuli/stressTest/biology_full.wav","./../stress_images/biology.gif",4,2);
+var blanket = new StressStimuli( "./../audio_stimuli/stressTest/blanket_full.wav","./../stress_images/blanket.gif",2,1);
+var critical = new StressStimuli( "./../audio_stimuli/stressTest/critical_full.wav","./../stress_images/critical.gif",3,1);
+var energy = new StressStimuli( "./../audio_stimuli/stressTest/energy_full.wav","./../stress_images/energy.gif",3,1);
+var evaporate = new StressStimuli( "./../audio_stimuli/stressTest/evaporate_full.wav","./../stress_images/evaporate.gif",4,2);
+var paper = new StressStimuli( "./../audio_stimuli/stressTest/paper_full.wav","./../stress_images/paper.gif",2,1);
+var politician = new StressStimuli( "./../audio_stimuli/stressTest/politician_full.wav","./../stress_images/politician.gif",4,3);
+var procedure = new StressStimuli( "./../audio_stimuli/stressTest/procedure_full.wav","./../stress_images/procedure.gif",3,2);
+var reputation = new StressStimuli( "./../audio_stimuli/stressTest/reputation_full.wav","./../stress_images/reputation.gif",4,3);
+var today = new StressStimuli( "./../audio_stimuli/stressTest/today_full.wav","./../stress_images/today.gif",2,2);
+var tomato = new StressStimuli( "./../audio_stimuli/stressTest/tomato_full.wav","./../stress_images/tomato.gif",3,2);
 
 /*array of stimuli*/
 var stressStimuli = [believe, biology, blanket, critical, energy, evaporate, paper, politician, procedure, reputation, today, tomato];
@@ -52,7 +52,7 @@ var createMap1 = function(){
 
 	var area1 = document.createElement("area");
 	area1.shape = "rect";
-	area1.coords = "0,0,200,300";
+	area1.coords = "0,0,155,300";
 	area1.href = "#";
 	area1.onclick = function(){
 		console.log("I clicked on the first syllable");
@@ -60,7 +60,7 @@ var createMap1 = function(){
 
 	var area2 = document.createElement("area");
 		area2.shape = "rect";
-		area2.coords = "200,0,400,300";
+		area2.coords = "155,0,400,300";
 		area2.href = "#";
 		area2.onclick = function(){
 			console.log("I clicked on the second syllable");
@@ -72,14 +72,46 @@ var createMap1 = function(){
 	stressMap1.appendChild(area2);
 };
 
+var createMap2 = function(){
+	var stressMap2 = document.createElement("map");
+	stressMap2.name = "stressMap2";
+
+	var area1 = document.createElement("area");
+	area1.shape = "rect";
+	area1.coords = "0,0,100,300";
+	area1.href = "#";
+	area1.onclick = function(){
+		console.log("I clicked on the first syllable");
+	};
+
+	var area2 = document.createElement("area");
+		area2.shape = "rect";
+		area2.coords = "100,0,200,300";
+		area2.href = "#";
+		area2.onclick = function(){
+			console.log("I clicked on the second syllable");
+	};
+
+	var area3 = document.createElement("area");
+		area3.shape = "rect";
+		area3.coords = "200,0,300,300";
+		area3.href = "#";
+		area3.onclick = function(){
+			console.log("I clicked on the third syllable");
+	};
+
+	stressImg.useMap = "#stressMap2";
+	stress_test_image.appendChild(stressMap2);
+	stressMap2.appendChild(area1);
+	stressMap2.appendChild(area2);
+	stressMap2.appendChild(area3);
+
+};
 
 
 
 
 
-
-var stressMap2 = document.createElement("map");
-stressMap2.name = "stressMap2";
 
 var stressMap3 = document.createElement("map");
 stressMap3.name = "stressMap3";
@@ -103,7 +135,16 @@ var stressTest = function(){
 	/* Inserts Image*/
 	stressImg.src = sample.image;
 	stress_test_image.appendChild(stressImg);
-	createMap1();
+
+	if(sample.numSyllables === 2){
+
+		createMap1();
+
+	}else{
+		createMap2();
+	}
+
+
 	
 };
 
