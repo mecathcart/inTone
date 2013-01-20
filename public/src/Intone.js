53b6477d6c6ed609e56bb50fa675b213d9f1519f
Emmy Cathcart <maryellencathcart@gmail.com>
2013-01-20 08:36:14 -0500

    practice stimuli now plays one of each condition.

--- public/src/Intone.js
+++ public/src/Intone.js
@@ -7,14 +7,17 @@ var intoneAudio = document.createElement("audio");
 intoneAudio.id = "intone_stimuli";
 audio_div.appendChild(intoneAudio);
 
+var complimentCount = 0;
+var stressCount = 0;
+var verbCount = 0;
 
 /*The function that is called when the practice button is clicked*/
 var practice = function(){
 
 	/*hide start button and instructions*/
 	$("#practice_instructions").hide();
-	$("#start_practice").hide()
-
+	$("#start_practice").hide();
+	
 	/*Go through stimuli*/
 	if(i < practiceStimuli.length){	
 
@@ -25,14 +28,34 @@ var practice = function(){
 			intoneAudio.src = sample.audio;
 			playAudioFile("intone_stimuli");
 		}else{
-			if(sample.hasOwnProperty('compliment')){
-				intoneAudio.src = sample.audioI;
-				playAudioFile("intone_stimuli");
-			}else{
+			if(sample.category == 'compliment'){
+				if(complimentCount < 1){
+					intoneAudio.src = sample.audioI;
+					playAudioFile("intone_stimuli");
+					complimentCount++
+				}else{
 				intoneAudio.src = sample.audioN;
 				playAudioFile("intone_stimuli");
+				}
+			}else if(sample.category == 'stressAdj'){
+				if(stressCount < 1){
+					intoneAudio.src = sample.audioI;
+					playAudioFile("intone_stimuli");
+					stressCount++
+				}else{
+				intoneAudio.src = sample.audioN;
+				playAudioFile("intone_stimuli");
+				}
+			}else{
+				if(verbCount < 1){
+					intoneAudio.src = sample.audioN;
+					playAudioFile("intone_stimuli");
+					verbCount++
+				}else{
+				intoneAudio.src = sample.audioI;
+				playAudioFile("intone_stimuli");
+				}
 			}
-			
 		}
 
 		/*Insert continuations*/
